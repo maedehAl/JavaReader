ccbbe586ee05d52e092b5fefaceef2a2adcbbefd
M.Almasi <M.Almasi@sadad.co.ir>
2023-11-07 11:15:39 +0330

    first init

---
+++ src/main/java/com/example/JavaReader/JavaReaderApplication.java
@@ -0,0 +1,13 @@
+package com.example.JavaReader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaReaderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaReaderApplication.class, args);
+	}
+
+}
