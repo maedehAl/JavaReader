43ecd4a88fc216e9b6fad7f949128385e491c9d9
M.Almasi <M.Almasi@sadad.co.ir>
2023-11-10 13:32:05 +0330

    commit

---
+++ src/main/java/com/example/JavaReader/CSVReader.java
@@ -0,0 +1,7 @@
+package com.example.JavaReader;
+
+import java.io.File;
+
+public class CSVReader {
+
+}
--- src/main/java/com/example/JavaReader/JavaReaderApplication.java
+++ src/main/java/com/example/JavaReader/JavaReaderApplication.java
@@ -1,13 +1,29 @@
 package com.example.JavaReader;
 
-import org.springframework.boot.SpringApplication;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.*;
 
 @SpringBootApplication
 public class JavaReaderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JavaReaderApplication.class, args);
-	}
+    public static void main(String[] args) throws IOException {
+        File file = new File("Account.csv");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
+		FileWriter fileWriter=new FileWriter(file.getName(),true);
+		BufferedWriter bufferedWriter=new BufferedWriter(fileWriter);
+		bufferedWriter.write("hello world");
+		bufferedWriter.close();
+		System.out.println("Done");
+
+//        Reader in = ...;
+//        CSVFormat.EXCEL.withHeader("Col1", "Col2", "Col3").parse(in);
+
+
+   }
 
 }
